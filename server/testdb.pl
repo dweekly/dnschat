a4bb0abfac9594bedb1b9c710d936010c1fd3932
David E. Weekly <david@weekly.org>
2015-12-05 21:27:32 +0000

    Make tests a little more sophisticated, use Test::Simple

--- server/testdb.pl
+++ server/testdb.pl
@@ -1,14 +1,21 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 use Data::Dumper;
+
 use msgdb;
 
-print(Dumper($msgdb));
+use Test::Simple tests => 4;
+
+# seed the message database
 send_message_to_room(1234, "Hi there!", "dweekly", "ship2015");
-print(Dumper($msgdb));
 
-print(Dumper(get_messages_since(1231,"ship2014"))); # expect nothing
-print(Dumper(get_messages_since(1235,"ship2015"))); # expect nothing
-print(Dumper(get_messages_since(1231,"ship2015"))); # expect one msg
+($m,$err) = get_messages_since(1231, "ship2014");
+ok(scalar @{$m} == 0); # expect nothing
 
-die();
+($m,$err) = get_messages_since(1235, "ship2015");
+ok(scalar @{$m} == 0); # expect nothing
+
+($m,$err) = get_messages_since(1231, "ship2015");
+ok(scalar @{$m} == 1); # expect one msg back
+ok($m->[0]->{ts} == 1234); # expect ts preserved
+
